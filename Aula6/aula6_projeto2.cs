5fa1c524257a45f99fc43c5f58cac2f37fc6c5b2
LucasSantosRosendo <contatolucasrosendo@gmail.com>
2024-03-22 08:54:22 -0300

    Atualização e upload do exercício 2
    Unifiquei o nome dos arquivos e fiz o upload do exercício 2.

---
+++ Aula6/aula6_projeto2.cs
@@ -0,0 +1,29 @@
+
+using System;
+class HelloWorld {
+  static void Main() {
+      
+      Random numAleatorio = new Random();
+      int valorInteiro = numAleatorio.Next(1,100);
+      Console.WriteLine("=== Desafio da Adivinhação ===");
+      Console.WriteLine("Para vencer esse jogo você precisa adivinhar o número inteiro gerado entre 1 a 100.");
+      while (true){
+          Console.WriteLine("Digite a sua tentativa de um número entre 1 a 100: ");
+          int chute = Convert.ToInt32(Console.ReadLine());
+          if (chute<1 || chute>100){
+              Console.WriteLine("Por favor, digite um valor válido entre 1 a 100 apenas!");
+              
+          }
+      if (chute<valorInteiro){
+          Console.WriteLine("Chutou baixo!");
+      }
+      else if (chute>valorInteiro){
+          Console.WriteLine("Chutou alto!");
+      }
+      else {
+          Console.WriteLine("Parabéns! Você acertou o número gerado!");
+          break;
+      }
+      }
+      }
+  }
