f1252f1b55191d992bb92909db02a42c3d570936
LucasSantosRosendo <contatolucasrosendo@gmail.com>
2024-06-27 14:45:08 -0300

    AAA
    AAA

---
+++ Aula10/aula10_exercicio2.cpp
@@ -0,0 +1,39 @@
+using namespace std;
+#include <iostream>
+#include <list>
+
+int main ()
+{
+  list < int >numeros;
+  for (int i = 0; i < 100; i++)
+	{
+	  numeros.push_back (random () % 100);
+	}
+
+  numeros.sort ();
+  for (int num : numeros)
+	{
+	  cout << num << endl;
+	}
+	
+	for (auto it = numeros.begin (); it != numeros.end (); it++)
+	{
+	    if (*it%2 == 0)
+	    {
+	        it = numeros.erase(it);
+	        break;
+	    }
+	    
+	    else 
+	    {
+	        it++;
+	    }
+	}
+	
+	for (int num : numeros)
+	{
+	    cout << num << endl;
+	}
+
+  return 0;
+}
